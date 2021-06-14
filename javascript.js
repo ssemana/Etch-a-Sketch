929eace3ab04eb617d4d19615afbafe8babb2ccc
Shannon Semana <ssemana@gmail.com>
2021-06-14 11:57:36 -0700

    added black and rgb

--- javascript.js
+++ javascript.js
@@ -8,8 +8,9 @@ const buttonsContainer = document.querySelector('.buttons')
 function createDivs(col, rows){
     for (let i = 0; i < (col * rows); i++){
         const div = document.createElement('div')
-        container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
-        container.style.gridTemplateRows = `repeat(${rows}, ifr)`;
+        div.style.border = '1px solid red'
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('box')
         }
     }
@@ -29,3 +30,34 @@ function grayColor() {
     buttonsContainer.appendChild(btnGray).classList.add('btn')
 }
 grayColor()
+
+function blackColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnBlack.textContent = 'Black'
+    btnBlack.addEventListener('click' , () =>{
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            box.style.background = `black`;
+        }))
+    })
+
+    buttonsContainer.appendChild(btnBlack).classList.add('btn')
+}
+
+blackColor()
+
+function rgbColor() {
+    const boxs = container.querySelectorAll('.box')
+    btnRGB.textContent = 'RGB'
+    btnRGB.addEventListener('click' , () =>{
+        boxs.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 255)
+            let G = Math.floor(Math.random() * 255)
+            let B = Math.floor(Math.random() * 255)
+            box.style.background = `rgb(${R}, ${G}, ${B})`
+        }))
+    })
+
+    buttonsContainer.appendChild(btnRGB).classList.add('btn')
+}
+
+rgbColor()
